d03ef9268bd7f6eeaf9852db0ea06a5cb2468cb1
KaraKay <kara@badrobotgames.com>
2022-08-25 13:55:08 -0700

    one bit at a time

--- vars/getAllStages2.groovy
+++ vars/getAllStages2.groovy
@@ -3,5 +3,15 @@ import io.jenkins.blueocean.rest.impl.pipeline.PipelineNodeGraphVisitor
 import io.jenkins.blueocean.rest.impl.pipeline.FlowNodeWrapper
 
 def call(String name = 'human') {
-echo "Hello, ${name}."
+    echo "Hello, ${name}."  
+
+    echo 'Getting all stages'
+
+    // Get all pipeline nodes that represent stages
+    def visitor = new PipelineNodeGraphVisitor( build.rawBuild )
+    echo "${visitor}"
+
+    def stages = visitor.pipelineNodes.findAll{stage -> stage.type == FlowNodeWrapper.NodeType.STAGE }
+    echo "${stages}"
+    //return stages
 }
