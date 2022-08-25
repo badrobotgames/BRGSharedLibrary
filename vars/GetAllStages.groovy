fedf7aa1d3607b44cdb4c37634ea87a378e2cb36
KaraKay <kara@badrobotgames.com>
2022-08-25 13:07:42 -0700

    testing get all stages

--- vars/GetAllStages.groovy
+++ vars/GetAllStages.groovy
@@ -1,5 +1,12 @@
 #!/usr/bin/env groovy
 
-def call(String name = 'human') {
-  echo "Hello, ${name}."
+def call() {
+    def visitor = new io.jenkins.blueocean.rest.impl.pipeline.PipelineNodeGraphVisitor(currentBuild.rawBuild)
+    echo "${visitor}"
+    def stages = visitor.pipelineNodes.findAll{
+        it.type == FlowNodeWrapper.NodeType.STAGE
+    }
+    
+    echo "${stages}"
+    return stages
 }
