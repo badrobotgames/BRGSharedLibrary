33e2fd38ef96a6753026d53f2c61bfc4f38795eb
KaraKay <kara@badrobotgames.com>
2022-08-25 13:14:27 -0700

    trying to fix

--- vars/GetAllStages.groovy
+++ vars/GetAllStages.groovy
@@ -1,7 +1,9 @@
 #!/usr/bin/env groovy
 
+import io.jenkins.blueocean.rest.impl.pipeline.PipelineNodeGraphVisitor
+
 def call() {
-    def visitor = new io.jenkins.blueocean.rest.impl.pipeline.PipelineNodeGraphVisitor(currentBuild.rawBuild)
+    def visitor = new PipelineNodeGraphVisitor(currentBuild.rawBuild)
     echo "${visitor}"
     def stages = visitor.pipelineNodes.findAll{ it ->
         it.type == FlowNodeWrapper.NodeType.STAGE
