9212fee009d4b244e1da8012f59a12a5a4f9f4b4
KaraKay <kara@badrobotgames.com>
2022-08-25 13:17:02 -0700

    getting rid of return for now?

--- vars/GetAllStages.groovy
+++ vars/GetAllStages.groovy
@@ -1,13 +1,13 @@
 #!/usr/bin/env groovy
-
 import io.jenkins.blueocean.rest.impl.pipeline.PipelineNodeGraphVisitor
+import io.jenkins.blueocean.rest.impl.pipeline.FlowNodeWrapper
+import org.jenkinsci.plugins.workflow.support.steps.build.RunWrapper
+import org.jenkinsci.plugins.workflow.actions.ErrorAction
 
 def call() {
-    def visitor = new PipelineNodeGraphVisitor(currentBuild.rawBuild)
-    echo "${visitor}"
-    def stages = visitor.pipelineNodes.findAll{ it ->
-        it.type == FlowNodeWrapper.NodeType.STAGE
-    }
-    
-    return stages
+    // Get all pipeline nodes that represent stages
+    def visitor = new PipelineNodeGraphVisitor( build.rawBuild )
+    def stages = visitor.pipelineNodes.findAll{ it.type == FlowNodeWrapper.NodeType.STAGE }
+    echo "${stages}"
+    //return stages
 }
