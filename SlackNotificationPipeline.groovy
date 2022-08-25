cf80153739c8b9354a6defe27c44cce9ab8a671e
KaraKay <kara@badrobotgames.com>
2022-08-25 13:40:47 -0700

    adding pipeline tester to here

---
+++ SlackNotificationPipeline.groovy
@@ -0,0 +1,50 @@
+@Library('BRGSharedLibrary')_
+
+pipeline
+{
+	agent any
+
+    stages
+    {
+		stage('Initialize')
+		{
+			steps
+			{			
+				script
+				{
+                    slackMessage = null
+                    stageStatus = [:]
+
+                    echo 'Hello world'
+                    sayHello 'Jenkins'
+
+                    GetAllStages
+				}
+			}
+		}
+	}
+}
+
+def SlackLog(message) 
+{
+	return SlackMessage(message, '32a852')
+}
+def SlackWarn(message) 
+{
+	return SlackMessage(message, 'e4be3a')
+}
+def SlackError(message) 
+{
+	return SlackMessage(message, 'CC1111')
+}
+def SlackMessage(message, color) 
+{
+    if(slackMessage)
+    {
+	    slackMessage = slackSend(channel: 'invasion-builds', message: "${env.JOB_NAME}_${env.BUILD_ID}: ${message}".toString(), color: "${color}".toString())
+    }
+}
+def UpdateSlackStatus()
+{
+
+}
