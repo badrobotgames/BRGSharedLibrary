7705b524ba1320baeb6dcf0ed9948dd7bf0f9f01
KaraKay <kara@badrobotgames.com>
2022-08-25 13:53:37 -0700

    checking that its not the imports

--- vars/getAllStages2.groovy
+++ vars/getAllStages2.groovy
@@ -1,4 +1,7 @@
 #!/usr/bin/env groovy
+import io.jenkins.blueocean.rest.impl.pipeline.PipelineNodeGraphVisitor
+import io.jenkins.blueocean.rest.impl.pipeline.FlowNodeWrapper
+
 def call(String name = 'human') {
 echo "Hello, ${name}."
 }
