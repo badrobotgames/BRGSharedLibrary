6d89af7bcf5d2e8195e50027f29fe3f434fc33e3
KaraKay <kara@badrobotgames.com>
2022-08-25 16:52:12 -0700

    fae r

--- vars/GetAdvancedStatusMessage.groovy
+++ vars/GetAdvancedStatusMessage.groovy
@@ -5,10 +5,13 @@ import io.jenkins.blueocean.rest.impl.pipeline.FlowNodeWrapper
 def call() {
 	def message = "${env.JOB_NAME}_${env.BUILD_ID} Status:"
 	// Get all pipeline nodes that represent stages
-	def visitor = new PipelineNodeGraphVisitor( currentBuild.rawBuild )
-	def stages = visitor.pipelineNodes.findAll{stage -> stage.type == FlowNodeWrapper.NodeType.STAGE }
+	def visitor = new PipelineNodeGraphVisitor(currentBuild.rawBuild)
+
+	visitor.getPipelineNodes().each{ blarg -> echo blarg }
+
+	//def stages = visitor.pipelineNodes.findAll{stage -> stage.type == FlowNodeWrapper.NodeType.STAGE }
     
-	visitor.pipelineNodes.each{ blarg -> echo blarg }
-    stages.each{ stage -> message = "${message}\n   - ${stage.displayName} : ${stage.status}" }
+	//visitor.pipelineNodes.each{ blarg -> echo blarg }
+    //stages.each{ stage -> message = "${message}\n   - ${stage.displayName} : ${stage.status}" }
 	return message
 }
