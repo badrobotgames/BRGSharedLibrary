cb9a24e302cb2715e3b1c3f39f704f8315640714
KaraKay <kara@badrobotgames.com>
2022-08-26 13:21:27 -0700

    fdsafdsf

--- SlackNotificationPipeline.groovy
+++ SlackNotificationPipeline.groovy
@@ -62,7 +62,7 @@ def SlackMessage(message, color)
 {
 	if(hasSlackMessage)
 	{
-		slackSend(channel: slackMessage.channelId, message: "${env.JOB_NAME}_${env.BUILD_ID}: ${message}".toString(), color: "${color}".toString())
+		slackSend(channel: slackResponse.threadId, message: "${env.JOB_NAME}_${env.BUILD_ID}: ${message}".toString(), color: "${color}".toString())
 	}
 	else
 	{
