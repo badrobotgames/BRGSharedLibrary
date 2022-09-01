88f4701ca3bf3c94889852b5c3774c7c789c2927
KaraKay <kara@badrobotgames.com>
2022-09-01 08:20:02 -0700

    trying with env vars

--- vars/BuildAndDeployBlackbox.groovy
+++ vars/BuildAndDeployBlackbox.groovy
@@ -5,16 +5,16 @@ def call()
 	script
 	{	
 		blackboxBuildChannel = env["${JOB_NAME}_BuildChannel"]		
-		bat("attrib -r ${blackboxGamePath}Config/Blackbox.ini".toString())
-		bat("${blackboxCLI} version add --name ${blackboxVersion} ${blackboxBaseParameters}".toString())								
-		bat("${blackboxCLI} version set --name ${blackboxVersion} ${blackboxBaseParameters}".toString())
+		bat("attrib -r ${env.blackboxGamePath}Config/Blackbox.ini".toString())
+		bat("${env.blackboxCLI} version add --name ${env.blackboxVersion} ${env.blackboxBaseParameters}".toString())								
+		bat("${env.blackboxCLI} version set --name ${env.blackboxVersion} ${env.blackboxBaseParameters}".toString())
 
-		bat("${UAT} BuildGraph -Script=BuildPipeline/BuildWindowsClient.xml -Target=\"BuildWindowsClient\" -set:UProjectPath=${env.PROJECT_PATH} -set:BuildNumber=${env.BUILD_NUMBER} -set:Configuration=${params.ClientConfiguration} -set:Iterate=${params.ITERATE_COOK} -set:StreamName=${P4Stream}")
+		bat("${env.UAT} BuildGraph -Script=BuildPipeline/BuildWindowsClient.xml -Target=\"BuildWindowsClient\" -set:UProjectPath=${env.PROJECT_PATH} -set:BuildNumber=${env.BUILD_NUMBER} -set:Configuration=${params.ClientConfiguration} -set:Iterate=${params.ITERATE_COOK} -set:StreamName=${P4Stream}")
 		
-		bat("${blackboxCLI} build register --platform-name ${BB_PlatformName} --platform-arch x64 ${blackboxBaseParameters}".toString())
-		bat("${blackboxCLI} build upload-binaries --entry-point Invasion.exe --game-archive ${blackboxBuildPath} ${blackboxBaseParameters} --no-log".toString())
+		bat("${env.blackboxCLI} build register --platform-name ${BB_PlatformName} --platform-arch x64 ${env.blackboxBaseParameters}".toString())
+		bat("${env.blackboxCLI} build upload-binaries --entry-point Invasion.exe --game-archive ${env.blackboxBuildPath} ${env.blackboxBaseParameters} --no-log".toString())
 	
-		bat("${blackboxCLI} build-channel update --stream-id ${blackboxBuildChannel} --as-head ${blackboxBaseParameters}".toString())
+		bat("${env.blackboxCLI} build-channel update --stream-id ${env.blackboxBuildChannel} --as-head ${env.blackboxBaseParameters}".toString())
 		slackUtils.PostToThread("Published client to Blackbox Launcher.".toString())
 	}
 }
