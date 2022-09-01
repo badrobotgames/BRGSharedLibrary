4039eea3633931a9189fd08730c7d20a83816f8f
KaraKay <kara@badrobotgames.com>
2022-09-01 08:25:11 -0700

    nope. back to copying things in

--- vars/BuildAndDeployBlackbox.groovy
+++ vars/BuildAndDeployBlackbox.groovy
@@ -4,6 +4,18 @@ def call()
 {
 	script
 	{	
+		UBT = 'Invasion/Engine/Build/BatchFiles/Build.bat'
+		UAT = 'Invasion/Engine/Build/BatchFiles/RunUAT.bat'
+
+		P4Stream=env["${JOB_NAME}_Stream"]
+		blackboxCLI = "${env.WORKSPACE}/Invasion/BuildPipeline/Blackbox/BlackBoxCLI.exe"
+		blackboxGamePath = "${env.WORKSPACE}/Invasion/Invasion/"
+		blackboxEnginePath = "${env.WORKSPACE}/Invasion/"
+		blackboxBuildPath = "${env.WORKSPACE}/Invasion/Invasion/Saved/StagedBuilds/WindowsClient/"
+		blackboxBaseParameters = "--namespace ${params.BB_Namespace} --apikey ${params.BB_APIKey} --game-engine ${blackboxEnginePath} --game-project ${blackboxGamePath}"
+		blackboxEditorVersion = "Editor_${env.BUILD_ID}_${env.P4_CHANGELIST}"
+		blackboxVersion = "${P4Stream}_${env.BUILD_ID}_${env.P4_CHANGELIST}"
+		
 		blackboxBuildChannel = env["${JOB_NAME}_BuildChannel"]		
 		bat("attrib -r ${env.blackboxGamePath}Config/Blackbox.ini".toString())
 		bat("${env.blackboxCLI} version add --name ${env.blackboxVersion} ${env.blackboxBaseParameters}".toString())								
